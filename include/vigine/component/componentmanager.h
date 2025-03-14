f0a2ea9aa64ebff2826cf0eed128f67b9c77f964
Vitalii Hromko <vitalii.hromko.dn@renesas.com>
2025-03-14 21:43:11 +0200

    Made ComponentManager class

--- include/vigine/component/componentmanager.h
+++ include/vigine/component/componentmanager.h
@@ -1,7 +1,89 @@
 #pragma once
 
-class ComponentManager {
+#include <unordered_map>
+#include <vector>
+#include <typeindex>
+#include <memory>
+#include <any>
+
+template<typename T>
+using ComponentUPtr = std::unique_ptr<T>;
+template<typename T>
+using ComponentContainer = std::vector<ComponentUPtr<T>>;
+
+class ComponentManager 
+{
 public:
     ComponentManager();
-    void manage();
+
+    template <typename T, typename... Args>
+    T* createComponent(Args&&... args) 
+    {
+        auto component = std::make_unique<T>(std::forward<Args>(args)...);
+        auto typeIndex = std::type_index(typeid(T));
+
+        if (_components.find(typeIndex) == _components.end())
+            _components[typeIndex] = std::move(std::make_any<ComponentContainer<T>>());
+
+        auto& container = std::any_cast<ComponentContainer<T>&>(_components[typeIndex]);
+        container.push_back(std::move(component));
+
+        return container.back().get();
+    }
+    
+    template <typename T>
+    auto cbegin() 
+    {
+        auto typeIndex = std::type_index(typeid(T));
+
+        auto it = _components.find(typeIndex);
+        if (it != _components.end()) 
+            return std::any_cast<ComponentContainer<T>&>(it->second).cbegin();
+
+        return getOrCreateEmptyContainer<T>().cbegin();
+    }
+
+    template <typename T>
+    auto cend() 
+    {
+        auto typeIndex = std::type_index(typeid(T));
+
+        auto it = _components.find(typeIndex);
+        if (it != _components.end())
+            return std::any_cast<ComponentContainer<T>&>(it->second).cend();
+
+        return getOrCreateEmptyContainer<T>().cend();
+    }
+
+
+    template <typename T>
+    void removeComponents() 
+    {
+        _components.erase(typeid(T));
+    }
+
+    void clear() 
+    {
+        _components.clear();
+        _empty.clear();
+    }
+
+private:
+    template <typename T>
+    const ComponentContainer<T>& getOrCreateEmptyContainer() 
+    {
+        auto typeIndex = std::type_index(typeid(T));
+
+        auto itEmpty = _empty.find(typeIndex);
+        if (itEmpty != _empty.end()) 
+            return std::any_cast<ComponentContainer<T>&>(itEmpty->second);
+
+        _empty[typeIndex] = std::move(std::make_any<ComponentContainer<T>>());
+
+        return std::any_cast<ComponentContainer<T>&>(_empty[typeIndex]);
+    }
+
+    private:
+        std::unordered_map<std::type_index, std::any> _components;
+        std::unordered_map<std::type_index, std::any> _empty;
 };
--- src/component/componentmanager.cpp
+++ src/component/componentmanager.cpp
@@ -5,6 +5,3 @@ ComponentManager::ComponentManager() {
     // Конструктор
 }
 
-void ComponentManager::manage() {
-    std::cout << "Managing components" << std::endl;
-}
